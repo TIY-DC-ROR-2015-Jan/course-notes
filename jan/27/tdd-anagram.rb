87c60c0255344b290da34cc609d9518afe517658
James Dabbs <jamesdabbs@gmail.com>
2015-01-27 10:49:24 -0500

    Reasonable anagram implementation

---
+++ jan/27/tdd-anagram.rb
@@ -0,0 +1,20 @@
+require "minitest/autorun"
+# require "minitest/reporters"
+
+# reporter_options = { color: true }
+# Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
+
+def is_anagram? arr1, arr2
+  arr1.sort == arr2.sort
+end
+
+class TestAnagram < Minitest::Test
+  def test_it_can_work
+    assert is_anagram? [:c,:a,:t], [:a,:c,:t]
+  end
+
+  def test_it_checks_more_than_length
+    # assert !is_anagram?
+    refute is_anagram? [:c,:a,:t], [:b,:a,:t]
+  end
+end
