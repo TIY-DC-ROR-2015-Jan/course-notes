535f0d50dfe6b5f3688c0501a340b57c2ab9f06a
James Dabbs <jamesdabbs@gmail.com>
2015-01-28 10:02:34 -0500

    Working word finder with tests

--- jan/28/scrabble-practice.rb
+++ jan/28/scrabble-practice.rb
@@ -1,35 +1,44 @@
 require "minitest/autorun"
 require 'pry'
- 
+
 #assumes finding all words from set of 3-letter-set
- 
-def possible_words letter
-    possibilities = []
-    combinations = letter.permutation.to_a + letter.permutation(2).to_a + letter.permutation(1).to_a
-    combinations.each  do  |x|
-    	possibilities.push(x.join)
-    end
-    return possibilities
-end
- 
-def word_finder
-    dictionary = ["cat", "at", "bat", "act", "a", "ta"]
-    results = []
-    possible_words.each do |x|
-	if dictionary.include?(x)
-		results.push(x)
-	end
-    end
-    return results
-end
- 
-class TestScrabble < Minitest::Test
-  #Trying to define letter argument from possible_words
-  
-  letter = ["c", "a", "t"]
- 
-  def test_is_string
-  word_finder.each.class == String
+
+def possible_words letters
+  possibilities = []
+  combinations = letters.permutation.to_a + letters.permutation(2).to_a + letters.permutation(1).to_a
+  combinations.each  do  |x|
+    possibilities.push(x.join)
   end
- 
+  return possibilities
+end
+
+def word_finder letters_to_try
+  # %w(cat at bat act a ta)
+  dictionary = ["cat", "at", "bat", "act", "a", "ta"]
+  results = []
+  possible_words(letters_to_try).each do |x|
+    if dictionary.include?(x)
+      results.push(x)
+    end
+  end
+  return results
+end
+
+class TestScrabble < Minitest::Test
+  def test_possible_words
+    possibilites = possible_words(["c","a","t"])
+    assert possibilites.include?("tca")
+  end
+  
+  def test_is_string
+    found_words = word_finder(["c","a","t"])
+    assert_equal found_words.length, 5
+    assert found_words.include?("act")
+    assert !found_words.include?("bat")
+  end
+
+  def test_with_a_b
+    found_words = word_finder(["t","b","a"])
+    assert found_words.include?("bat")
+  end
 end
