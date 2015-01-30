358af7e31bb6e05cc9f51a69f062db99ff338211
James Dabbs <jamesdabbs@gmail.com>
2015-01-30 08:44:43 -0500

    Implement a better turn function

--- jan/29/pig.rb
+++ jan/29/pig.rb
@@ -1,35 +1,29 @@
 # TODO: Track player names?
 
-# You have to roll once
 current_player = 1
 max_score = 10
 p1_score = 0
 p2_score = 0
 
-def roll_dice total_so_far
-  roll = rand(1..6)
+def take_turn
+  turn_total = 0
+  loop do
+    roll = rand(1..6)
+    puts "You rolled a #{roll}."
 
-  # Decide to hold or roll
-  puts "You rolled a #{roll}."
-  if roll == 1
-  	# Turn over
-    puts "Bad news :("
-    return 0
-  else
-    total_so_far += roll
-    puts "Roll again (turn total so far #{total_so_far})? (y/n)"
-    decision = gets.chomp
-    if decision == "y"
-      return roll_dice(total_so_far)
-    else # held, get all of the running total points to keep
-      return total_so_far
-      # it should now be player 2's turn
+    if roll == 1
+      puts "Bad news :("
+      return 0
+
+    else
+      turn_total += roll
+
+      puts "Roll again (turn total so far #{turn_total})? (y/n)"
+      if gets.chomp == "n"
+        return turn_total
+      end
     end
   end
-end
-
-def take_turn
-  roll_dice 0
 end
 
 # until somebody wins
