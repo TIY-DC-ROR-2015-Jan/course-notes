c97725fcd8cdf7f0101f44c013013c53919d1fbe
James Dabbs <jamesdabbs@gmail.com>
2015-02-02 11:22:13 -0500

    Add Dog class

---
+++ feb/02/dogs/dog.rb
@@ -0,0 +1,10 @@
+class Dog
+	def initialize name
+		@name = name
+	end
+
+  # attr_reader :name
+	def name
+		@name
+	end
+end
--- feb/02/dogs/dog_spec.rb
+++ feb/02/dogs/dog_spec.rb
@@ -1,10 +1,16 @@
 require "minitest/autorun"
 
 require_relative "./person"
+require_relative "./dog"
 
 class TestAllTheThings < Minitest::Test
   def test_people_know_their_name
     person = Person.new "james"
     assert_equal "james", person.name
+  end
+
+  def test_dogs_know_their_name
+    dog = Dog.new "rufus"
+    assert_equal "rufus", dog.name
   end
 end
