6a8a9d10e13441bd74494816ebee7fcb0d9098b3
James Dabbs <jamesdabbs@gmail.com>
2015-02-03 11:48:29 -0500

    Working smart AI

--- feb/03/guessing/play_game.rb
+++ feb/03/guessing/play_game.rb
@@ -2,13 +2,14 @@ require './game'
 require './player'
 
 g = Game.new
-p = DumbAI.new
+p = SmartAI.new
 
 until g.over?
 	guess = p.get_guess
 	puts "You guessed: #{guess}"
-	
+
 	response = g.check_guess guess
+	p.update_guess response
 	puts response
 end
 
--- feb/03/guessing/player.rb
+++ feb/03/guessing/player.rb
@@ -10,3 +10,23 @@ class DumbAI
 		rand 1..100
 	end
 end
+
+class SmartAI
+	def initialize
+		@low = 0
+		@high = 100
+	end
+
+	def get_guess
+		new_guess = (@low + @high) / 2
+		@last_guess = new_guess
+	end
+
+	def update_guess response
+		if response.include? "high"
+			@high = @last_guess
+		elsif response.include? "low"
+			@low = @last_guess
+		end
+	end
+end
