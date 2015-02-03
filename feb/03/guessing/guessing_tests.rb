4c0253547392571b58b50bc5cba874a38017ba03
James Dabbs <jamesdabbs@gmail.com>
2015-02-03 10:28:21 -0500

    Loop 1

--- feb/03/guessing/guessing_tests.rb
+++ feb/03/guessing/guessing_tests.rb
@@ -7,4 +7,9 @@ class GuessingTests < MiniTest::Test
 		g = Game.new
 		assert_equal g.guesses_left, 6
 	end
+
+	def test_it_has_picked_an_answer
+		g = Game.new
+		assert g.answer.is_a? Integer
+	end
 end
