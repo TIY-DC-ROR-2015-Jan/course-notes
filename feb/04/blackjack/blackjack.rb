5e67d032dc949abed8dcd28b5949278a350a66d7
James Dabbs <jamesdabbs@gmail.com>
2015-02-04 16:40:28 -0500

    These are my blackjack solutions.
    There are many like them, but these are mine.

---
+++ feb/04/blackjack/blackjack.rb
@@ -0,0 +1,77 @@
+class Card
+  SUITS = %i(S H D C)
+  RANKS = (2..10).to_a + %i(J Q K A)
+
+  attr_reader :rank, :suit
+
+  def initialize rank, suit
+    @rank, @suit = rank, suit
+  end
+
+  def value
+    case @rank
+    when :A
+      1
+    when :K, :Q, :J
+      10
+    else
+      @rank
+    end
+  end
+
+  def to_s
+    "#{@rank}#{@suit}"
+  end
+end
+
+class Deck
+  attr_reader :cards, :drawn
+
+  def initialize
+    @cards, @drawn = [], []
+    Card::SUITS.each do |suit|
+      Card::RANKS.each do |rank|
+        @cards.push Card.new(rank, suit)
+      end
+    end
+    @cards.shuffle!
+  end
+
+  def draw
+    card = @cards.pop
+    @drawn.push card
+    card
+  end
+end
+
+class Hand
+  def initialize
+    @cards = []
+  end
+
+  def add *cards
+    @cards += cards
+  end
+
+  def value
+    base = @cards.reduce(0) { |sum,card| sum + card.value }
+
+    if base <= 11 && @cards.any? { |card| card.rank == :A }
+      base + 10
+    else
+      base
+    end
+  end
+
+  def blackjack?
+    value == 21 && @cards.count == 2
+  end
+
+  def busted?
+    value > 21
+  end
+
+  def to_s
+    @cards.map { |card| card.to_s }.join ", "
+  end
+end
