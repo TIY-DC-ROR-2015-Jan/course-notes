d4b862da00ffa4188ee97c1919c1336e5ddec631
James Dabbs <jamesdabbs@gmail.com>
2015-02-05 14:13:43 -0500

    Add neighbor finding logic

--- feb/05/grid.rb
+++ feb/05/grid.rb
@@ -4,6 +4,7 @@ require 'pry'
 
 class Grid
   def initialize row_count, col_count
+    @row_count, @col_count = row_count, col_count
     @board = []
     row_count.times do
       row = []
@@ -14,6 +15,7 @@ class Grid
       end
       @board.push row
     end
+    update_neighbors!
   end
 
   def display
@@ -34,14 +36,52 @@ class Grid
   end
 
   def take_turn!
+    @board.each do |row|
+      row.each do |cell|
+        cell.take_turn!
+      end
+    end
+  end
+
+  def in_bounds? i,j
+    i >= 0 && i < @row_count && j >= 0 && j < @col_count
+  end
+
+  def neighbors i,j
+    [
+      [i,j+1],
+      [i,j-1],
+      [i+1,j+1],
+      [i+1,j-1],
+      [i-1,j+1],
+      [i-1,j-1],
+      [i+1,j],
+      [i-1,j]
+    ].select { |a,b| in_bounds? a,b }
+  end
+
+  def update_neighbors!
+    @row_count.times do |i|
+      @col_count.times do |j|
+        current_cell = @board[i][j]
+
+        positions = neighbors(i,j)
+        positions.each do |a,b|
+          current_cell.neighbors.push @board[a][b]
+        end
+      end
+    end
   end
 end
 
 if $PROGRAM_NAME == __FILE__
-  grid = Grid.new 3, 3
+  grid = Grid.new 40, 40
 
-  #1000.times do
+  10000.times do |i|
+    system "clear"
+    puts "Turn #{i}"
     grid.display
     grid.take_turn!
-  #end
+    sleep 0.1
+  end
 end
