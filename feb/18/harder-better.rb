a0ed7dc36d016abfdffb73508bc8b422ef3fddd9
James Dabbs <jamesdabbs@gmail.com>
2015-02-18 11:32:58 -0500

    Each sledgehammer

--- feb/18/harder-better.rb
+++ feb/18/harder-better.rb
@@ -14,33 +14,40 @@ class MyApp < Sinatra::Base
   # delete '/second' do
   #   "DELETEd to /second"
   # end
-  post '/harder' do
-    # ?v=2 as a query param
-    if params[:v].to_i == 2 # they asked for v=2
-      play "Harder2"
-    else
-      play "Harder1"
+  # post '/harder' do
+  #   # ?v=2 as a query param
+  #   if params[:v].to_i == 2 # they asked for v=2
+  #     play "Harder2"
+  #   else
+  #     play "Harder1"
+  #   end
+  # end
+
+  # post '/better' do
+  #   play "Better1"
+  # end
+  # post '/better/:variant' do
+  #   # version comes in as part of path
+  #   if params[:variant].to_i == 2
+  #     play "Better2"
+  #   else
+  #     play "Better1"
+  #   end
+  # end
+
+  # post '/faster' do
+  #   play "Faster1"
+  # end
+
+  # post '/stronger' do
+  #   play "Stronger1"
+  # end
+  words = %w( After Better DoIt Ever Faster Harder Hour MakeIt MakesUs MoreThan Never Our Over Stronger WorkIs WorkIt )
+  words.each do |w|
+    post "/#{w.downcase}" do
+      variant = params[:v] || 1
+      play "#{w}#{variant}"
     end
-  end
-
-  post '/better' do
-    play "Better1"
-  end
-  post '/better/:variant' do
-    # version comes in as part of path
-    if params[:variant].to_i == 2
-      play "Better2"
-    else
-      play "Better1"
-    end
-  end
-
-  post '/faster' do
-    play "Faster1"
-  end
-
-  post '/stronger' do
-    play "Stronger1"
   end
 end
 
