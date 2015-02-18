c1555bb96a2c27d47885e1d21f70a04054c90afe
James Dabbs <jamesdabbs@gmail.com>
2015-02-18 11:29:44 -0500

    Add other variant

--- feb/18/harder-better.rb
+++ feb/18/harder-better.rb
@@ -15,6 +15,7 @@ class MyApp < Sinatra::Base
   #   "DELETEd to /second"
   # end
   post '/harder' do
+    # ?v=2 as a query param
     if params[:v].to_i == 2 # they asked for v=2
       play "Harder2"
     else
@@ -24,6 +25,14 @@ class MyApp < Sinatra::Base
 
   post '/better' do
     play "Better1"
+  end
+  post '/better/:variant' do
+    # version comes in as part of path
+    if params[:variant].to_i == 2
+      play "Better2"
+    else
+      play "Better1"
+    end
   end
 
   post '/faster' do
